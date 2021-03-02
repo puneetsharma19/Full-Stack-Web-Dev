62e07a29f1cc49687c367f1cb7abdb265180b78d
puneet <puneetsharma190799@gmail.com>
2021-03-03 00:04:43 +0530

    functions in js
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

--- 4.) CSS-3/js(css3 webinar3).js
+++ 4.) CSS-3/js(css3 webinar3).js
@@ -17,3 +17,57 @@ function a (){
 // in javascript we can treat functions as variables
 console.log(a, typeof a)
 console.log(a(), typeof a())
+
+
+
+//more about functions
+console.log(beta()) // works perfectly fine before defining the function
+// js works in a two pass way, that is the js interpreter stores the function definitions inside the memory 
+
+/* HOISTING:
+    when the javascript interpreter executes this page, it will take all the function definitions 
+    and place them at the top of the page so that the functions are already defined before they are called.
+    Inside the debugger we will see no changes in the code, but that is what the  js interpreter does
+*/
+
+
+// But for the below case:
+    //ERROR !
+// let gamma = function(){
+//     return "C"
+// }
+// NOTE : for a variable to be used (any type of variable), it needs to be declared first before getting used
+
+
+function alpha(){
+    return "A"
+}
+
+function beta(){
+    return "B"
+}
+
+// Polymorphism in JS eg:
+function area(height, width){ // no need to specify data types here
+    return height*width
+}
+
+console.log('area(3,4) = ', area(3,4))
+
+/* NOTE : if we write area(4) output :- NaN. I f we do not pass all the arguments, the unpassed arguments
+          become undefined. */
+
+/* now if we write function as : 
+
+    function area(height, width){ 
+        if(typeof width === undefined){
+            return Math.PI*height*height
+        }
+        return height*width
+    }
+
+// NOTE : false, 0, null and undefined also act as undefined inside an if case so we can write,
+    if(!width){
+        return Math.PI*height*height
+    }
+*/
