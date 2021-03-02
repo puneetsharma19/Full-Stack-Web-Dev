d5bf67113df4e0a51698678f4e36127b0c1db8a4
puneet <puneetsharma190799@gmail.com>
2021-03-02 23:32:49 +0530

    javascript data types
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

---
+++ 4.) CSS-3/js(css3 webinar3).js
@@ -0,0 +1,19 @@
+// Data types in JS
+
+console.log(33,typeof 33)
+console.log(typeof 234.465)
+console.log(typeof 'a')
+console.log(typeof 'awqd')
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+// functions in JAVASCRIPT
+
+function a (){
+    return 1
+}
+
+// in javascript we can treat functions as variables
+console.log(a, typeof a)
+console.log(a(), typeof a())
