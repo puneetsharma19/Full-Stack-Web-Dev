0aba7b975d7113de750e06beeb2347a8923c7e46
puneet <puneetsharma190799@gmail.com>
2021-03-05 18:32:51 +0530

    this keyword
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

--- 6.)JS-2/jsfile1.js
+++ 6.)JS-2/jsfile1.js
@@ -29,3 +29,42 @@ x('param2')
 */
 
 
+
+//this pointer in Javascript
+/*
+        >'this' points to the place where the function has been called
+        > this can be called outside a function as well, and in that case, this points to Global scope
+*/
+
+function checkThis (){
+    console.log(this) // here this points to the window because we have called the function through the window
+}
+
+let obj = {
+    a: 10,
+    b: 'asbad',
+ // 10 : 'ajsk' to access this, we can write obj['10']; 10 will be reated as a string
+ // it will give "ajsk" as output
+ // for any member of class, we can access it either through '.' operator or obj['a']
+ // obj['a'] will give output 10   
+    c: true,
+    d: function () {
+        console.log(this) //here 'this' points to obj
+    },
+    e: {
+        l:234,
+        m: 'sdkjh',
+        n: function(){
+            console.log(this)
+        }
+    }
+}
+
+/* IMPORTANT NOTE : The 'this' pointer is binded on run time, i.e if in above case, if function d is called via obj, 
+                    i.e obj.d will run console.log(this) and here this will poin to obj, but,
+                    if we do let z = obj.d and then do z(), it will print window because d is not called via
+                    an object here and window is the current scope
+*/
+
+
+
