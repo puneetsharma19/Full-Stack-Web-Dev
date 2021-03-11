b6b80f90722d535bb8eaee1c153a7b0afafceadc
puneet <puneetsharma190799@gmail.com>
2021-03-11 10:15:51 +0530

    hi
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

---
+++ 6.)JS-2/webinar2.js
@@ -0,0 +1,24 @@
+
+let str = "qenwi"
+let num = 223
+let bool = true
+let arr = [246,234,9,2840]
+let obj = {a:10, b:"wriuad"}
+let fun = function() {console.log('hello')}
+
+// if x and y are not primitive,
+//  and x == y comes out to be true <- what does that mean ?
+//this means they are references to the same object in memory
+
+console.log(typeof String) // > type of String comes out to be function
+//String is a function String()
+
+
+//all the below are functions
+console.log('typeOf String', typeof String)
+console.log('typeOf Boolean', typeof Boolean)
+console.log('typeOf Number', typeof Number)
+console.log('typeOf Array', typeof Array)
+console.log('typeOf Object', typeof Object)
+console.log('typeOf Function', typeof Function)
+
