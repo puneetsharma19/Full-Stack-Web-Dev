22c6c76c0925e998c1e7e2a7423a87c613f57173
puneet <puneetsharma190799@gmail.com>
2021-04-10 02:15:21 +0530

    slot machine
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

--- 9.) PROJECT -01 - CASINO SLOT MACHINE/script.js
+++ 9.) PROJECT -01 - CASINO SLOT MACHINE/script.js
@@ -1,7 +1,6 @@
-
 let value1 = document.getElementById('value1')
-let value2 = document.getElementById('value2')
-let value3 = document.getElementById('value3')
+    let value2 = document.getElementById('value2')
+    let value3 = document.getElementById('value3')
 
 values = ['😂', '😃', '😎', '😍', '🤑', '🤑', '😭', '😢']
 
@@ -9,9 +8,23 @@ function getRandomValue (){
     return values[Math.floor(Math.random()*7)]
 }
 
-setInterval(() => {
-    value1.innerText = getRandomValue()
-    value2.innerText = getRandomValue()
-    value3.innerText = getRandomValue()
-},300)
+
+let startBtn = document.getElementById('startBtn')
+
+startBtn.onclick = function(){
+    
+    value1.style.animationPlayState = "running";
+    value2.style.animationPlayState = "running";
+    value3.style.animationPlayState = "running";
+
+    setInterval(() => {
+        value1.innerText = getRandomValue()
+        value2.innerText = getRandomValue()
+        value3.innerText = getRandomValue()
+    },300)
+        
+}
+
+
+
 
