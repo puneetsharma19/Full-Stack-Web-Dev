990a104b2673d5332cebdb43f6bba255627d164e
puneet <puneetsharma190799@gmail.com>
2021-04-10 14:02:40 +0530

    slot machine
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

--- 9.) PROJECT -01 - CASINO SLOT MACHINE/script.js
+++ 9.) PROJECT -01 - CASINO SLOT MACHINE/script.js
@@ -1,11 +1,13 @@
+
+
 let value1 = document.getElementById('value1')
     let value2 = document.getElementById('value2')
     let value3 = document.getElementById('value3')
 
-values = ['😂', '😃', '😎', '😍', '🤑', '🤑', '😭', '😢']
+values = ['😂', '😂','😂', '😃', '😃', '😃', '😎', '😎', '😎','😍', '🤑', '🤑', '😭', '😢', '🤑']
 
 function getRandomValue (){
-    return values[Math.floor(Math.random()*7)]
+    return values[Math.floor(Math.random()*14)]
 }
 
 
@@ -17,13 +19,28 @@ startBtn.onclick = function(){
     value2.style.animationPlayState = "running";
     value3.style.animationPlayState = "running";
 
-    setInterval(() => {
-        value1.innerText = getRandomValue()
-        value2.innerText = getRandomValue()
-        value3.innerText = getRandomValue()
-    },300)
-        
+    let count = 0;
+    let myVar = setInterval(() => {
+
+            count += 1;
+            if(count === 30)
+                clearInterval(myVar)
+            value1.innerText = getRandomValue()
+            value2.innerText = getRandomValue()
+            value3.innerText = getRandomValue()
+        },100)
+
 }
+
+
+
+if(value1.innerText === value2.innerText && value2.innerText === value3.innerText)
+        alert("YOU WIN !")
+
+
+
+        
+
 
 
 
