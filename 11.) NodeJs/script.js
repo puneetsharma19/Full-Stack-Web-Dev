be715867f8a97cb0ec613548b48d67d062c3ab44
puneet <puneetsharma190799@gmail.com>
2021-04-24 00:53:31 +0530

    node and express js
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

--- 11.) NodeJs/script.js
+++ 11.) NodeJs/script.js
@@ -13,3 +13,40 @@ fs.readFile(__dirname + '/myFile.txt',function(err,data){
     if(err) throw err
     console.log(data.toString())
 })
+
+//assignment
+fs.writeFile(__dirname+'/ass.txt',"cat \ndog \ncat \nrat \nmonkey \ndog \n", function(err){
+    if(err) throw err
+    console.log("File created")
+})
+
+
+
+fs.readFile(__dirname + '/ass.txt',function(err,data){
+    if(err) throw err
+
+    let str = data.toString()
+    str = str.split('\n');
+    var countOccurrences = arr => arr.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev),{});
+    var arr = countOccurrences(str)
+    delete arr['']   
+    
+    for(let val in arr){
+        
+        fs.writeFile(__dirname + '/copied.txt', val,function(err){
+            if(err) throw err
+            console.log("File successfully written")
+        })
+    }
+    
+    
+
+})
+
+
+
+
+
+
+
+
