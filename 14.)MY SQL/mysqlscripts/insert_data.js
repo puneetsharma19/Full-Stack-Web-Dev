e0c83fe66cc176b8fe4032933744697c45e1f0f5
puneet <puneetsharma190799@gmail.com>
2021-04-29 00:44:14 +0530

    mysql with nodejs
    Signed-off-by: puneet <puneetsharma190799@gmail.com>

---
+++ 14.)MY SQL/mysqlscripts/insert_data.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'mytestdb',
+    user: 'myuser',
+    password: 'mypass'
+})
+
+const insert = {
+    name:process.argv[2],
+    age:parseInt(process.argv[3]),
+    city:process.argv[4]
+}
+
+
+connection.query(
+    `INSERT INTO persons (name, age, city) VALUES(
+        '${insert.name}',
+        ${insert.age},
+        '${insert.city}'
+    )`,
+    function(err, results){
+        if(err){
+            console.error(err)
+        }else{
+            console.log("Inserted successfully")
+            console.log(results)
+        }
+        connection.close()
+    }
+)
